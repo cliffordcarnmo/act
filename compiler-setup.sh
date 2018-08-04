2b6974d903b23dc7c2504b7b7cff1f2f1f7c78fd
Clifford Carnmo <clifford@carnmo.com>
2018-08-04 21:09:06 +0200

    Adding VBCC-related source to avoid remote dependencies

--- compiler-setup.sh
+++ compiler-setup.sh
@@ -1,14 +1,13 @@
-VBCC_SOURCE=http://server.owl.de/~frank/tags/vbcc0_9fP1.tar.gz
-VASM_SOURCE=http://sun.hasenbraten.de/vasm/release/vasm.tar.gz
-VLINK_SOURCE=http://sun.hasenbraten.de/vlink/release/vlink.tar.gz
+VBCC=vbcc_src
+VBCC_SOURCE=vbcc0_9fP1.tar.gz
+VASM_SOURCE=vasm.tar.gz
+VLINK_SOURCE=vlink.tar.gz
 
-curl -O $VBCC_SOURCE
-curl -O $VASM_SOURCE
-curl -O $VLINK_SOURCE
+rm -r bin
 
-tar -xf ${VBCC_SOURCE##*/}
-tar -xf ${VASM_SOURCE##*/}
-tar -xf ${VLINK_SOURCE##*/}
+tar -xf ${VBCC}/${VBCC_SOURCE}
+tar -xf ${VBCC}/${VASM_SOURCE}
+tar -xf ${VBCC}/${VLINK_SOURCE}
 
 cd vbcc && mkdir bin && TARGET=m68k make && cd ..
 cd vasm && CPU=m68k SYNTAX=mot make && cd ..
@@ -18,10 +17,6 @@ mkdir bin
 cp vbcc/bin/dtgen vbcc/bin/vbccm68k vbcc/bin/vc vbcc/bin/vprof bin/
 cp vasm/vasmm68k_mot vasm/vobjdump bin/
 cp vlink/vlink bin/
-
-rm ${VBCC_SOURCE##*/}
-rm ${VASM_SOURCE##*/}
-rm ${VLINK_SOURCE##*/}
 
 rm -r vbcc
 rm -r vasm
